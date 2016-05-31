0bf9399b744858611aa9d79ad92f9c0b4745a6a1
Daniel <daniel865livegmail.com>
2016-05-30 21:00:04 -0500

    Merge master fixed conflict

--- src/com/mimos/inventario/dao/ConnectionFactory.java
+++
@@ -1,7 +0,0 @@
-package com.mimos.inventario.dao;
-
-/**
- * Created by Daniel on 29/05/2016.
- */
-public class ConnectionFactory {
-}
--- src/com/mimos/inventario/entities/Computer.java
+++
@@ -1,7 +0,0 @@
-package com.mimos.inventario.entities;
-
-/**
- * Created by Daniel on 29/05/2016.
- */
-public class Computer {
-}
